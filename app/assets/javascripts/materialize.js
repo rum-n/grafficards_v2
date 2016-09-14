5ed5d008d91df420c5d9832a4cc2f886f2479746
rum-n <manev.ru@gmail.com>
2016-09-14 19:29:34 +0300

    First commit

---
+++ app/assets/javascripts/materialize.js
@@ -0,0 +1,14 @@
+                    <script>
+                        $(document).ready(function() {
+                            $(".button-collapse").sideNav();
+                        }
+                        )
+                        
+
+                        $(document).ready(function(){
+                            $('.collapsible').collapsible({
+                            accordion : false 
+                        });
+                    });
+
+                    </script>
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+	def home
+	end
+
+	def about
+	end
+
+	def contact
+	end
+
+	def blog
+	end
+
+	def gallery
+	end
+
+	def message_input 
+	end
+
+	def address_input
+	end
+
+	def payment
+	end
+
+	def card_sent
+	end
+
+	def page_not_found
+	end
+end
