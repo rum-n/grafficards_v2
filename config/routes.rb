83c0fc7f8c7669cebf480f50b5e168ee9c042a0b
rum-n <manev.ru@gmail.com>
2016-09-15 22:35:29 +0300

    Contact Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  #resources :posts
+  resources :contacts, only: [:new, :create]
+
   root 'pages#home'
 
   get '/about', to: 'pages#about'
   get '/blog', to: 'pages#blog'
-  get '/contact', to: 'pages#contact'
+  get '/contact', to: 'contacts#new'
   get '/gallery', to: 'pages#gallery'
   get '/message_input', to: 'pages#message_input'
   get '/address_input', to: 'pages#address_input'
